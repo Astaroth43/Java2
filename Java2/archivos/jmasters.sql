7afad8d52d7068602dfed7657c4df56dcdb069b4
mike140 <micky_1408sands@hotmail.com>
2015-04-22 17:29:46 -0500

    BDfinal

--- Java2/archivos/jmasters.sql
+++ Java2/archivos/jmasters.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4.2
+-- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
--- Servidor: localhost
--- Tiempo de generación: 21-04-2015 a las 03:36:57
--- Versión del servidor: 5.6.13
--- Versión de PHP: 5.4.17
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 23-04-2015 a las 00:02:24
+-- Versión del servidor: 5.5.39
+-- Versión de PHP: 5.4.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `jmasters`
 --
-CREATE DATABASE IF NOT EXISTS `jmasters` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `jmasters`;
 
 -- --------------------------------------------------------
 
@@ -29,15 +27,21 @@ USE `jmasters`;
 --
 
 CREATE TABLE IF NOT EXISTS `cliente` (
-  `ID` int(4) NOT NULL AUTO_INCREMENT,
-  `NOMBRE` varchar(50) DEFAULT NULL,
-  `APELLIDO` varchar(50) DEFAULT NULL,
-  `SEXO` enum('Masculino','Femenino') DEFAULT NULL,
-  `DIRECCION` varchar(100) DEFAULT NULL,
-  `TELEFONO` varchar(10) DEFAULT NULL,
-  `RFC` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+`ID` int(4) NOT NULL,
+  `NOMBRE` varchar(50) NOT NULL,
+  `APELLIDO` varchar(100) NOT NULL,
+  `SEXO` enum('Masculino','Femenino') NOT NULL,
+  `DIRECCION` varchar(200) NOT NULL,
+  `TELEFONO` varchar(10) NOT NULL,
+  `RFC` varchar(30) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`ID`, `NOMBRE`, `APELLIDO`, `SEXO`, `DIRECCION`, `TELEFONO`, `RFC`) VALUES
+(1, 'Miguel Angel', 'Silva Martínez', 'Masculino', 'García Diego 735', '8333954', '1234');
 
 -- --------------------------------------------------------
 
@@ -46,16 +50,14 @@ CREATE TABLE IF NOT EXISTS `cliente` (
 --
 
 CREATE TABLE IF NOT EXISTS `empleado` (
-  `ID` int(4) NOT NULL AUTO_INCREMENT,
-  `NOMBRE` varchar(50) DEFAULT NULL,
-  `APELLIDO` varchar(50) DEFAULT NULL,
+`ID` int(4) NOT NULL,
+  `NOMBRE` varchar(50) NOT NULL,
+  `APELLIDO` varchar(100) NOT NULL,
   `SEXO` enum('Masculino','Femenino') NOT NULL,
-  `TURNO` enum('Matutino','Vespertino') NOT NULL,
-  `PUESTO` enum('Gerente','Limpieza','Cajero') NOT NULL,
-  `DIRECCION` varchar(100) DEFAULT NULL,
-  `TELEFONO` varchar(10) DEFAULT NULL,
-  `RFC` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
+  `SALARIO` float NOT NULL,
+  `DIRECCION` varchar(200) NOT NULL,
+  `TELEFONO` varchar(10) NOT NULL,
+  `RFC` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -65,15 +67,55 @@ CREATE TABLE IF NOT EXISTS `empleado` (
 --
 
 CREATE TABLE IF NOT EXISTS `producto` (
-  `ID` int(4) NOT NULL AUTO_INCREMENT,
+`ID` int(4) NOT NULL,
   `TIPO` enum('Computadoras','Accesorios','Consumibles','Servicios') NOT NULL,
-  `NOMBRE` varchar(50) NOT NULL,
+  `NOMBRE` varchar(100) NOT NULL,
   `PRECIO` float NOT NULL,
   `CANTIDAD` int(10) NOT NULL,
-  `DESCRIPCION` text NOT NULL,
-  PRIMARY KEY (`ID`)
+  `DESCRIPCION` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+MODIFY `ID` int(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+MODIFY `ID` int(4) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+MODIFY `ID` int(4) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
